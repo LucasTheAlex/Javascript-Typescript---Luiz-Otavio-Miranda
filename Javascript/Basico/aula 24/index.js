37d3e073253bdd47fe772664fb21ee3d6960fdb2
SERAPHIM@ROOT <counitzero@gmail.com>
2026-09-21 17:44:22 -0300

    Add aula 24 form exercise
    This commit adds a small HTML form for collecting a person's name, surname, weight, and height. The JavaScript defines a `pessoa` factory function, stores submitted entries in an array, and prevents the form from reloading while capturing the input values on submit.

---
+++ Javascript/Basico/aula 24/index.js
@@ -0,0 +1,29 @@
+const pessoa = (nome, sobrenome, peso, altura) => {
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        peso: peso,
+        altura: altura
+    }
+}
+
+let listaPessoas = [];
+
+function meuEscopo() {
+    const form = document.querySelector('.form');
+    
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+        
+        console.log(nome.value, sobrenome.value, peso.value, altura.value);
+        listaPessoas.push(pessoa(nome.value, sobrenome.value, peso.value, altura.value));
+    }
+    
+    form.addEventListener('submit', recebeEventoForm);
+}
+
+meuEscopo();
