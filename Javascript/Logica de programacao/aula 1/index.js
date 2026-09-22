974aa74c85ba6b2bbea64b759964f68d3143163e
LucasTheAlex <counitzero@gmail.com>
2026-09-21 23:55:01 -0300

    Aulas de javascript

---
+++ Javascript/Logica de programacao/aula 1/index.js
@@ -0,0 +1,19 @@
+/**
+ * Operadores de comparação
+ * > maior que
+ * < menor que
+ * <= menor ou igual
+ * >= maior ou igual
+ * == igualdade (valor)
+ * === igualdade estrita (valor e tipo)
+ * != diferente (valor)
+ * !== diferente estrito (valor e tipo)
+ */
+
+const exprecao = 10 < 5;
+console.log(exprecao);
+
+const num1 = 10;
+const num2 = '10';
+
+console.log(num1 === num2);
---
+++ Javascript/Logica de programacao/aula 2/index.js
@@ -0,0 +1,17 @@
+/**
+ * Operadores logicos
+ * && -> AND
+ * || -> OR
+ * ! -> NOT
+ */
+
+// const expressao = true && true;
+// const expressao = false || true;
+
+// console.log(expressao);
+
+const usuario = 'Luiz';
+const senha = '123456789';
+
+const vaiLogar = usuario === 'Luiz' && senha === '1234567';
+console.log(!!vaiLogar);
